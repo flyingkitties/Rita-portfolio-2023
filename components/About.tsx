2e73935fe306577dc50772e8267b73f455996239
Rita Guilherme <ritaguilherme@Ritas-MacBook-Air.local>
2023-06-14 11:05:09 +0100

    V14.6.2

--- components/About.tsx
+++ components/About.tsx
@@ -34,26 +34,31 @@ function About({}: Props) {
         animate={{}}
         loading="lazy"
         src="https://live.staticflickr.com/65535/52488679029_6382831232_m.jpg"
-        className=" w-24 h-24 sm:w-40 sm:h-40 rounded-full -mb-20 md:mb-0 flex-shrink-0 
+        className=" w-20 h-20 sm:w-40 sm:h-40 rounded-full -mb-20 md:mb-0 flex-shrink-0 
         object-cover md:w-64 md:h-64 lg:h-[320px] lg:w-[320px] mt-16 sm:mt-10 "
       />
 
-      <div className="space-y-5 px-5 md:px-10 lg:px-20 lg:space-y-10">
-        <h4 className="md:text-4xl sm:text-3xl text-2xl font-semibold py-5 ">
+      <div className="space-y-5 px-5 sm:px-12 md:px-10 lg:px-20 lg:space-y-10">
+        <h4 className="md:text-4xl sm:text-3xl text-2xl font-semibold py-2 sm:py-5 ">
           Here is a{' '}
           <span className="underline decoration-[#f7AB0A]/50">litle</span>
           {''} {''}background
         </h4>
-        <p className=" font-light text-gray-400 tracking-[1px] text-xs sm:text-sm md:text-base text-justify lg:tracking-[3px]">
-          Over 3 years experience in product management and UI/UX Design, during
-          this time I developed a better understanding of the process as well as
-          managing a team.
+        <p className=" font-light text-gray-400 tracking-wide text-xs sm:text-base text-justify lg:tracking-[3px]">
+          Dedicated and versatile Web Developer with over 3 years experience
+          including product management and UI/UX Design.
         </p>
-        <p className=" font-light text-gray-400 tracking-[1px] text-xs sm:text-sm md:text-base  text-justify lg:tracking-[3px] ">
+        <p className=" font-light text-gray-400 tracking-wide text-xs sm:text-base text-justify lg:tracking-[3px]">
           Over the past 2 years I have focused on Full-stack Web Development and
           fell in love with the challenge. Problem solving is truly one of my
-          passions. I&apos;m eager to lean more and collaborate in rapidly
-          changing environments.
+          passions.
+        </p>
+
+        <p className=" font-light text-gray-400 tracking-wide text-xs sm:text-base  text-justify lg:tracking-[3px] ">
+          I&apos;m proficient in managing teams, eager to lean and collaborate
+          in rapidly changing environments. I possess an excellent can-do
+          attitude and thrive in fast-paced settings. Highly motivated, reliable
+          and proactive with a strong work ethic.
         </p>
       </div>
     </motion.div>
--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -24,68 +24,69 @@ const ContactMe = (props: Props) => {
     <div
       className="h-screen relative flex flex-col md:flex-row 
     text-center md:text-left items-center mx-auto justify-evenly
-    px-10 max-w-7xl">
+    px-10 max-w-7xl ">
       <h3
         className="absolute top-24 uppercase tracking-[20px]
        text-gray-500 text-2xl ">
         Contact Me
       </h3>
 
-      <div className=" absolute top-36  flex flex-col space-y-3 pt-10 px-10">
-        <h4 className=" text-2xl sm:text-4xl font-semibold text-center">
+      <div className=" absolute top-36  flex flex-col space-y-3 pt-10 mx-56 sm:px-10">
+        <h4 className=" text-xl sm:text-4xl font-semibold text-center">
           Get in touch
         </h4>
-        <p className="text-l sm:text-xl text-center font-extralight">
+        <p className=" sm:text-xl text-center font-extralight">
           I&apos;m currently open to new opportunities.
         </p>
-        <p className="text-xl text-center ">Let&apos;s talk</p>
+        <p className="text-lg sm:text-xl text-center ">Let&apos;s talk</p>
 
-        <div className="space-y-5 pt-5 px-10 sm:px-0 ">
-          <div className="flex text-left space-x-5 ">
+        <div className="space-y-2 sm:space-y-5 pt-2 sm:pt-5 px-32 sm:px-0 text-xs ">
+          <div className="flex text-left space-x-5 items-center">
             <EnvelopeIcon className="text-[#f7ab0a]/60 hover:text-[#f7ab0a]/70 h-7 w-7  " />
             <p>Email me</p>
           </div>
-          <div className="flex text-left space-x-5 pb-5">
+          <div className="flex text-left space-x-5 pb-2 sm:pb-5 items-center">
             <MapPinIcon className="text-[#f7ab0a]/60 hover:text-[#f7ab0a]/70 h-7 w-7  " />
             <p>Brighton</p>
           </div>
         </div>
+        <div className="">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className=" flex flex-col space-y-5 ">
+            <div className="flex  sm:space-x-2  ">
+              <input
+                {...register('name')}
+                placeholder="Name"
+                className="contactInput "
+                type="text"
+              />
+              <input
+                {...register('email')}
+                placeholder="Email"
+                className="contactInput"
+                type="email"
+              />
+            </div>
 
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className=" flex flex-col space-y-5 w-fit  ">
-          <div className="flex space-x-2 ">
             <input
-              {...register('name')}
-              placeholder="Name"
+              {...register('subject')}
+              placeholder="Subject"
               className="contactInput"
               type="text"
             />
-            <input
-              {...register('email')}
-              placeholder="Email"
+            <textarea
+              {...register('message')}
+              placeholder="Message"
               className="contactInput"
-              type="email"
             />
-          </div>
-
-          <input
-            {...register('subject')}
-            placeholder="Subject"
-            className="contactInput"
-            type="text"
-          />
-          <textarea
-            {...register('message')}
-            placeholder="Message"
-            className="contactInput"
-          />
-          <button
-            type="submit"
-            className="bg-[#f7ab0a]/60 py-3 rounded-lg text-[rgb(48,48,48)] font-bold hover:bg-[#f7ab0a]/70">
-            Submit
-          </button>
-        </form>
+            <button
+              type="submit"
+              className="bg-[#f7ab0a]/60 py-3 rounded-lg text-[rgb(48,48,48)] font-bold hover:bg-[#f7ab0a]/70">
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
