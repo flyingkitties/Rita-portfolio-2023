2c2e36ac35ecd346cd52e3cd9e5ee8cf4c6bf78e
Rita Guilherme <ritaguilherme@Ritas-MacBook-Air.local>
2023-06-22 09:55:27 +0100

    Fix on padding icons

--- components/ProjectCards.tsx
+++ components/ProjectCards.tsx
@@ -26,7 +26,7 @@ const ProjectCards = ({ projectData }: any) => {
     space-y-7 hover:opacity-100 flex-shrink-0 w-[300px] sm:w-[450px] mt-10 p-7 text-center 
     opacity-75  transition-opacity duration-200  hover:-translate-y-2">
       <div className="w-full h-full p-3 mx-10 space-y-7">
-        <div className="absolute flex h-25 w-25 top-0 right-0 ">
+        <div className="absolute flex h-25 w-25 top-1 right-1 ">
           <SocialIcon
             className="hover:bg-white hover:rounded-full"
             url={url1}
