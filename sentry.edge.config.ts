3822dfceda106683877b1c1c0480bc2413589adb
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-03-11 14:50:16 +0000

    sentry sample rate change

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,13 +3,13 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://572a1ff45cbcaa151c2e37a4c618d1d1@o4506869430026240.ingest.us.sentry.io/4506892436111360",
+  dsn: 'https://572a1ff45cbcaa151c2e37a4c618d1d1@o4506869430026240.ingest.us.sentry.io/4506892436111360',
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
